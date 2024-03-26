c558bfdc037ecb20fa68b5213343a37413c738ef
Aintza03 <115790075+Aintza03@users.noreply.github.com>
2024-03-26 20:37:03 +0100

    Revert "Merge branch 'Haizea' into main"
    This reverts commit 54deba9145600f6a1f67ad98cd24955b34b488c2, reversing
    changes made to 3795cbe7b1700ac2fcb1a5c3f78d2a931ed817c0.

--- maven-client-projectoCalidad/src/main/java/com/ikea/app/client/ClientMain.java
+++ maven-client-projectoCalidad/src/main/java/com/ikea/app/client/ClientMain.java
@@ -11,9 +11,7 @@ import javax.ws.rs.core.Response.Status;
 
 import com.ikea.app.pojo.Cliente;
 import com.ikea.app.client.window.ClientRegistration;
-
 import com.ikea.app.client.window.ClientLogin;
-import com.ikea.app.client.window.ProductList;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -49,8 +47,7 @@ public class ClientMain{
 		String port = args[1];
 
 		ClientMain clientMain = new ClientMain(hostname, port);
-		ClientRegistration window = new ClientRegistration(clientMain.webTarget);
+		ClientRegistration window_registration = new ClientRegistration(clientMain.webTarget);
 		ClientLogin window_login = new ClientLogin(clientMain.webTarget);
-		ProductList window2 = new ProductList(clientMain.webTarget);
 	}
 }
