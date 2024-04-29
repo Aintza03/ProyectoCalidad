1ffb81ec1f2510dbbb2482954de7c96eec75ae06
Aintza03 <115790075+Aintza03@users.noreply.github.com>
2024-04-29 13:32:06 +0200

    Update AdminLoginControllerTest.java

--- src/test/java/com/ikea/app/client/controller/AdminLoginControllerTest.java
+++ src/test/java/com/ikea/app/client/controller/AdminLoginControllerTest.java
@@ -3,6 +3,7 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static org.junit.Assert.assertNull;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.Invocation;
 import javax.ws.rs.client.WebTarget;
@@ -11,6 +12,8 @@ import javax.ws.rs.core.Response;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockedStatic;
 import org.mockito.MockitoAnnotations;
 import com.ikea.app.pojo.Admin;
 import javax.ws.rs.core.Response.Status;
@@ -18,18 +21,25 @@ import static org.mockito.Mockito.*;
 import com.ikea.app.client.controller.AdminLoginController;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
+import org.mockito.Answers;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
 public class AdminLoginControllerTest{
     private AdminLoginController controllerTest;
     @Mock
-    private WebTarget webTarget = mock(WebTarget.class);
+    private Client client;
+
+    @Mock(answer=Answers.RETURNS_DEEP_STUBS)
+    private WebTarget webTarget;
+    
     @Mock
     Admin admin = mock(Admin.class);
-    @Mock
-    private Response response = mock(Response.class);
+   
     @Mock
     private Invocation.Builder invocation = mock(Invocation.Builder.class);
     @Captor
     private ArgumentCaptor<Entity<Admin>> AdminEntityCaptor;
+    
     @Before
     public void setUp() {
         //Obligatorio con Mockito
@@ -38,19 +48,40 @@ public class AdminLoginControllerTest{
     }
     @Test
     public void testLoginAdmin() {
-        //Cuando se expecifique el path en el web target devolvera el webTarget
+        try (MockedStatic<ClientBuilder> clientBuilder = Mockito.mockStatic(ClientBuilder.class)) {
+            clientBuilder.when(ClientBuilder::newClient).thenReturn(client);
+            when(client.target("http://localhost:8080/rest/resource")).thenReturn(webTarget);
+        }
+        Admin adminResponse = new Admin();
+        adminResponse.setUsuario("Alfredo");
+        adminResponse.setContrasena("1234");
+        Response response = mock(Response.class);
         when(webTarget.path("loginAdmin")).thenReturn(webTarget);
-        //Cuando se especifique el request(MediaTypeApplication) devolvera la invocation
         when(webTarget.request(MediaType.APPLICATION_JSON)).thenReturn(invocation);
-        when(admin.getUsuario()).thenReturn("Alfredo");
-        when(admin.getContrasena()).thenReturn("1234");
         when(invocation.post(any(Entity.class))).thenReturn(response);
         when(response.getStatus()).thenReturn(Status.OK.getStatusCode());
-        when(response.readEntity(Admin.class)).thenReturn(admin);
-        //Se ejecuta la funcion de loginAdmin pero nunca se accede a la base de datos, los when especifican que hay que devolver en cada llamada
-        assertTrue(controllerTest.loginAdmin(webTarget,"Alfredo","1234").equals(admin));
+        when(response.readEntity(Admin.class)).thenReturn(adminResponse);
+        Admin admin = controllerTest.loginAdmin(webTarget,"Alfredo", "1234");
         verify(webTarget.request(MediaType.APPLICATION_JSON)).post(AdminEntityCaptor.capture());
         assertEquals("Alfredo", AdminEntityCaptor.getValue().getEntity().getUsuario());
         assertEquals("1234", AdminEntityCaptor.getValue().getEntity().getContrasena());
+    }
+    @Test
+    public void testLoginAdminError() {
+        try (MockedStatic<ClientBuilder> clientBuilder = Mockito.mockStatic(ClientBuilder.class)) {
+            clientBuilder.when(ClientBuilder::newClient).thenReturn(client);
+            when(client.target("http://localhost:8080/rest/resource")).thenReturn(webTarget);
+        }
+        Admin adminResponse = new Admin();
+        adminResponse.setUsuario("Alfredo");
+        adminResponse.setContrasena("1234");
+        Response response = mock(Response.class);
+        when(webTarget.path("loginAdmin")).thenReturn(webTarget);
+        when(webTarget.request(MediaType.APPLICATION_JSON)).thenReturn(invocation);
+        when(invocation.post(any(Entity.class))).thenReturn(response);
+        when(response.getStatus()).thenReturn(Status.BAD_REQUEST.getStatusCode());
+        when(response.readEntity(Admin.class)).thenReturn(adminResponse);
+        assertNull(controllerTest.loginAdmin(webTarget,"Alfredo", "1234"));
+        verify(webTarget.request(MediaType.APPLICATION_JSON)).post(AdminEntityCaptor.capture());
     }
 }
