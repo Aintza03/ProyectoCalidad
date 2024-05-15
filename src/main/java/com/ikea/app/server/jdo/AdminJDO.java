f68ef968f399670ba0a9c3bf62aa6d739429485b
haizeagc <116098300+haizeagc@users.noreply.github.com>
2024-05-15 20:18:41 +0200

    Documentación de JDO

--- src/main/java/com/ikea/app/server/jdo/AdminJDO.java
+++ src/main/java/com/ikea/app/server/jdo/AdminJDO.java
@@ -6,47 +6,77 @@ import java.util.Set;
 import javax.jdo.annotations.Column;
 import java.util.HashSet;
 @PersistenceCapable
+/**Clase que representa a un vendedor en la base de datos.
+ */
 public class AdminJDO {
     @PrimaryKey
+    /**Usuario del vendedor. */
     String usuario = null;
+    /**Contrasena del vendedor. */
     String contrasena = null;
     @Column(name = "vendedor")
+    /**Lista de productos que vende el vendedor. */
     Set<ProductoJDO> lista;
 
+    /**Constructor de la clase.
+     * @param usuario Usuario del vendedor.
+     * @param contrasena Contrasena del vendedor.
+     */
     public AdminJDO(String usuario, String contrasena) {
         this.usuario = usuario;
         this.contrasena = contrasena;
         this.lista = new HashSet<ProductoJDO>();
     }
+    /**Constructor de la clase.
+     */
     public AdminJDO() {
         this.lista = new HashSet<ProductoJDO>();
     }
-
+    /**Metodo que devuelve el usuario del vendedor.
+     * @return Usuario del vendedor.
+     */
     public String getUsuario() {
         return this.usuario;
     }
+    /**Metodo que devuelve la lista de productos que vende el vendedor.
+     * @return Lista de productos que vende el vendedor.
+     */
     public Set<ProductoJDO> getLista() {
         return this.lista;
     }
+    /**Metodo que devuelve la contrasena del vendedor.
+     * @return Contrasena del vendedor.
+     */
     public String getContrasena() {
         return this.contrasena;
     }
-
+      /**Metodo que modifica el usuario del vendedor.
+     * @param usuario Nuevo usuario del vendedor.
+     */
     public void setUsuario(String usuario) {
         this.usuario = usuario;
     }
-
+    /**Metodo que modifica la lista de productos que vende el vendedor.
+     * @param lista Nueva lista de productos que vende el vendedor.
+     */
     public void setLista(Set<ProductoJDO> lista) {
         this.lista = lista;
     }
-
+    /**Metodo que modifica la contrasena del vendedor.
+     * @param contrasena Nueva contrasena del vendedor.
+     */
     public void setContrasena(String contrasena) {
         this.contrasena = contrasena;
     }
+    /**Metodo que anade un producto a la lista de productos que vende el vendedor.
+     * @param producto Producto que se va a anadir a la lista.
+     */
     public void anadirLista(ProductoJDO producto) {
         lista.add(producto);
     }
-
+    /**Metodo que devuelve la representacion en String del Admin.
+	 * @return Representacion en String del Admin.
+	 */
     public String toString() {
         return "Usuario: " + this.usuario;
     }
--- src/main/java/com/ikea/app/server/jdo/HistorialJDO.java
+++ src/main/java/com/ikea/app/server/jdo/HistorialJDO.java
@@ -2,31 +2,57 @@ package com.ikea.app.server.jdo;
 import java.util.*;
 import javax.jdo.annotations.*;
 @PersistenceCapable
+/**Clase que representa a un historial en la base de datos.
+ */
 public class HistorialJDO{
     @PrimaryKey
+    /**Cliente al que pertenece el historial. */
     public ClienteJDO cliente;
     @Persistent
     @Column(name = "productosHistorial")
+    /**Productos que contiene el historial. */
     public Set<ProductoJDO> productos;
+    /**Constructor de la clase.
+     * @param cliente Cliente al que pertenece el historial.
+     */
     public HistorialJDO(ClienteJDO cliente){
         this.cliente = cliente;
         this.productos = new HashSet<ProductoJDO>();
     }
+     /**Metodo para añadir un nuevo producto al historial.
+     * @param producto Nuevo producto a añadir.
+     */
     public void addProducto(ProductoJDO producto){
         this.productos.add(producto);
     }
+     /**Metodo que modifica la lista de productos.
+     * @param producto Nueva lista de productos.
+     */
     public void setProductos(Set<ProductoJDO> productos){
         this.productos = productos;
     }
+    /**Metodo que devuelve los productos que contiene el historial.
+     * @return Productos que contiene el historial.
+     */
     public Set<ProductoJDO> getProductos(){
         return this.productos;
     }
+     /**Metodo que devuelve el cliente.
+     * @return Cliente que contiene el historial.
+     */
     public ClienteJDO getCliente(){
         return this.cliente;
     }
+        /**Metodo que modifica el cliente.
+        * @param cliente Nuevo cliente.
+        */
     public void setCliente(ClienteJDO cliente){
         this.cliente = cliente;
     }
+   
+    /**Metodo que devuelve la representacion en String del Historial.
+	 * @return Representacion en String del Historial.
+	 */
     public String toString(){
         return "Historial: " + this.cliente.toString() + " " + this.productos.toString();
     }
