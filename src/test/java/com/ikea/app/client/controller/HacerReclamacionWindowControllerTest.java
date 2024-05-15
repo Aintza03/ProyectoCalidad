c32747a50f2ee9f607d148652b665ac7aa181005
Aintza03 <115790075+Aintza03@users.noreply.github.com>
2024-05-15 18:35:44 +0200

    Update HacerReclamacionWindowControllerTest.java
    Co-Authored-By: Javi9Deusto <115790076+Javi9Deusto@users.noreply.github.com>
    Co-Authored-By: haizeagc <116098300+haizeagc@users.noreply.github.com>

--- src/test/java/com/ikea/app/client/controller/HacerReclamacionWindowControllerTest.java
+++ src/test/java/com/ikea/app/client/controller/HacerReclamacionWindowControllerTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
 import static org.mockito.Mockito.*;
-
+import java.util.*;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
@@ -18,14 +18,14 @@ import org.junit.Before;
 import org.junit.Test;
 
 import javax.ws.rs.core.Response.Status;
-
+import javax.ws.rs.core.GenericType;
 import com.ikea.app.client.controller.CestaWindowController;
 import com.ikea.app.pojo.Reclamacion;
 import com.ikea.app.pojo.Cliente;
 import com.ikea.app.pojo.Producto;
 import com.ikea.app.pojo.Admin;
-
 import javax.ws.rs.ProcessingException;
+
 public class HacerReclamacionWindowControllerTest{
     
     private HacerReclamacionWindowController controllerTest;
@@ -76,19 +76,45 @@ public class HacerReclamacionWindowControllerTest{
     }
 
     @Test
-    public void sendReclamationTest3() {
+    public void sendReclamationTest() {
         //Cuando se expecifique el path en el web target devolvera el webTarget
         when(webTarget.path("sendReclamation")).thenReturn(webTarget);
         //Cuando se especifique el request(MediaTypeApplication) devolvera la invocation
-        when(webTarget.queryParam("admin", reclamacion.getAdmin().getUsuario())).thenReturn(webTarget);
+        when(webTarget.queryParam("admin", admin.getUsuario())).thenReturn(webTarget);
         when(webTarget.request(MediaType.APPLICATION_JSON)).thenReturn(invocation);
-        when(invocation.post(any(Entity.class))).thenReturn(response);
-        when(response.getStatus()).thenReturn(Status.BAD_REQUEST.getStatusCode());
-        when(response.readEntity(Reclamacion.class)).thenReturn(reclamacion);
-        //Se ejecuta la funcion de vaciarCesta pero nunca se accede a la base de datos, los when especifican que hay que devolver en cada llamada
-        assertNull(controllerTest.sendReclamation(webTarget, admin));
+        when(invocation.get()).thenReturn(response);
+        when(response.getStatusInfo()).thenReturn(Status.OK);
+        when(response.getStatusInfo().toEnum()).thenReturn(Status.OK);
+        GenericType<List<Reclamacion>> listType = new GenericType<List<Reclamacion>>(){};
+        List<Reclamacion> reclamaciones = new ArrayList<Reclamacion>();
+        when(response.readEntity(listType)).thenReturn(reclamaciones);
+        assertTrue(controllerTest.sendReclamation(webTarget, admin).equals(reclamaciones));
     }
-
+    @Test
+    public void sendReclamationTest2() {
+        //Cuando se expecifique el path en el web target devolvera el webTarget
+        when(webTarget.path("sendReclamation")).thenReturn(webTarget);
+        //Cuando se especifique el request(MediaTypeApplication) devolvera la invocation
+        when(webTarget.queryParam("admin", admin.getUsuario())).thenReturn(webTarget);
+        when(webTarget.request(MediaType.APPLICATION_JSON)).thenReturn(invocation);
+        when(invocation.get()).thenReturn(response);
+        when(response.getStatusInfo()).thenReturn(Status.BAD_REQUEST);
+        when(response.getStatusInfo().toEnum()).thenReturn(Status.BAD_REQUEST);
+        GenericType<List<Reclamacion>> listType = new GenericType<List<Reclamacion>>(){};
+        List<Reclamacion> reclamaciones = new ArrayList<Reclamacion>();
+        when(response.readEntity(listType)).thenReturn(reclamaciones);
+        assertTrue(controllerTest.sendReclamation(webTarget, admin).equals(reclamaciones));
+    }
+    @Test
+    public void sendReclamationTest3() {
+        //Cuando se expecifique el path en el web target devolvera el webTarget
+        when(webTarget.path("sendReclamation")).thenThrow(new ProcessingException("Error"));
+        //Cuando se especifique el request(MediaTypeApplication) devolvera la invocation
+        GenericType<List<Reclamacion>> listType = new GenericType<List<Reclamacion>>(){};
+        List<Reclamacion> reclamaciones = new ArrayList<Reclamacion>();
+        assertTrue(controllerTest.sendReclamation(webTarget, admin).equals(reclamaciones));
+    }
+    
     @Test
     public void resolverReclamacionTest() {
         //Cuando se expecifique el path en el web target devolvera el webTarget
